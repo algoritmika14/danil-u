dfee2ce9262ddd42f177e35c086d3df505150088
algoritmika14 <117454825+algoritmika14@users.noreply.github.com>
2022-11-04 16:38:05 +0300

    ответ на задание

---
+++ cricets.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cricets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("crickets");
+    }
+
+    // גûגמה פנאחû "crickets"
+    void Update()
+    {
+        print("crickets");
+        print("crickets");
+        print("crickets");
+        print("crickets");
+        print("crickets");
+        print("crickets");
+        print("crickets");
+        print("crickets");
+        print("crickets");
+    }
+}
